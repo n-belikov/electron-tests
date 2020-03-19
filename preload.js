8dd5c1de8eb63bab4893b71eea84abb50681885e
Nikita <evan.gelior@ya.ru>
2020-03-19 13:01:52 +0700

    (+) add button in html youtube

--- preload.js
+++ preload.js
@@ -2,11 +2,19 @@
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
+    const element = document.getElementById(selector);
     if (element) element.innerText = text
+  };
+
+  console.log(document.getElementById("leanback"));
+  console.log(document.getElementsByName("body"));
+  const leanback = document.getElementById("leanback");
+  if (leanback) {
+    leanback.innerHTML += "<button style='position: fixed; top: 0; left: 0; width: 200px; height: 200px;'></button>";
   }
 
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
-})
+
+});
--- renderer.js
+++ renderer.js
@@ -6,6 +6,12 @@
 // process.
 const electron = require('electron');
 
+const launchYoutubeTV = () => {
+  electron.remote.getCurrentWindow().loadURL("https://youtube.com/tv", {
+    userAgent: "Mozilla/5.0 (Web0S; Linux/SmartTV) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/5.2.1 Chr0me/38.0.2125.122 Safari/537.36 LG Browser/8.00.00(LGE; 60UH6550-UB; 03.00.15; 1; DTV_W16N); webOS.TV-2016; LG NetCast.TV-2013 Compatible (LGE, 60UH6550-UB, wireless)"
+  });
+};
+
 document.getElementById("youtube").addEventListener("click", () => {
-  electron.remote.getCurrentWindow().loadURL("https://youtube.com/");
+  launchYoutubeTV();
 });
